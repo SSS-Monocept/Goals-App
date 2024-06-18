a2ff5988b4f8b69368a577ce162303eca949830f
SSS-Monocept <shouryasinghsengar@monocept.com>
2024-06-18 16:23:23 +0530

    Created Skeleton for Goals App

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    
+    <View style={styles.appContainer}>
+      {/* 
+      1. By defalut the parent container will only take the minimum amount of space required to hold the children 
+      2. We need our parent to take the entire screen space 
+      */}
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goal !" />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  appContainer: {
+    flex: 1, //Enables the outter/parent container to take all the available screeen
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 6,
+  },
 });
