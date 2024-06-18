22defac93195ac6b9691f0b06c5594a22686adbf
SSS-Monocept <shouryasinghsengar@monocept.com>
2024-06-19 03:54:29 +0530

    Working on GoalInput File

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import { useState } from "react";
 import GoalItem from "./components/GoalItem";
-import { Button, StyleSheet, TextInput, View, FlatList } from "react-native";
+import GoalInput from "./components/GoalInput";
+import { StyleSheet, View, FlatList } from "react-native";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalState] = useState("");
   //Task 2: To manage the list of course goals
   const [courseGoals, setCourseGoals] = useState([]);
   const [goalCounter, setGoalCounter] = useState(0);
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalState(enteredText);
-  }
   //Task 1: to get the value enterd from the goalInputHandler to addGoalHandler.
   //Therefore, we need to store it as state, which is updated with every keystroke of goalInputHandler, so that we can use it at second fnction
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       { id: goalCounter.toString(), text: enteredGoalText },
@@ -27,14 +24,7 @@ export default function App() {
       1. By defalut the parent container will only take the minimum amount of space required to hold the children 
       2. We need our parent to take the entire screen space 
       */}
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your Course Goal !"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       {/* Task 3: To display the course added course goals on the */}
       <View style={styles.goalsContainer}>
         <FlatList
@@ -60,22 +50,6 @@ const styles = StyleSheet.create({
     flex: 1, //Enables the outter/parent container to take all the available screeen
     paddingTop: 50,
     paddingHorizontal: 16,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
   },
   goalsContainer: {
     flex: 6,
