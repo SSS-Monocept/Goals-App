f978ab9a86f941a9c83298b2953bde8833a82259
SSS-Monocept <shouryasinghsengar@monocept.com>
2024-06-19 15:38:54 +0530

    Adding Ripple Effect & ios alternative

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -4,14 +4,18 @@ function GoalItem(props) {
   return (
     //I have replaced .key with .id beacuse key should't be passed as props
     //bind() is a js function that pre allows you to pre-configuer a function for future execution
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      {/* Enables tap to delted item */}
-      <View style={styles.goalItem} key={props.id}>
+
+    <View style={styles.goalItem} key={props.id}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         {/* Rounded corner does;t work directly on ios, for htat wrap the text
             within view */}
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 export default GoalItem;
@@ -19,11 +23,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
